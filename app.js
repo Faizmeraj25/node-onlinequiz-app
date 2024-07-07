5f9702e844fba643234f44ef2e9c5f2a738186aa
Sheikh Faiz Meraj <faizmeraj25@gmail.com>
2024-07-07 22:21:32 +0530

    Added take quiz and footer functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose')
 const Quiz = require('./models/quiz')
+const bodyParser = require('body-parser')
 const app = express(); 
 
 const dB = require('./config/keys').mongoURI;
@@ -12,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Express body parser
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -27,10 +28,10 @@ app.get('/', (req, res)=>{
 
 app.get('/create-quiz', (req, res)=>{
     res.render('create-quiz')
-})
-app.post('/create-quiz', async (req, res)=>{
-    const { quizName, numQuestions, ...questionsData } = req.body;
-
+});
+app.post('/create-quiz',  async(req, res)=>{
+    const { quizName, numQuestions, ...questionsData } = await req.body;
+    console.log('req.body', quizName, numQuestions, questionsData);
     const questions = [];
 
     for (let i = 1; i <= numQuestions; i++) {
@@ -55,12 +56,38 @@ app.post('/create-quiz', async (req, res)=>{
     try {
         const savedQuiz = await newQuiz.save();
         console.log('Saved !!');
-        res.status(201).json(savedQuiz);
-        res.redirect('welcome');
+        // res.status(201).json(savedQuiz);
+        res.redirect('/');
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 }); 
+app.get('/quizzes', async (req, res)=>{
+        try {
+            const quizzes = await Quiz.find();
+            // res.status(200).json(quizzes);
+            console.log(quizzes); 
+            res.render('quizzes', {quizzes: quizzes});
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+});
+// Fetch a specific quiz by ID
+app.get('/quizzes/:id', async (req, res) => {
+    try {
+        const quiz = await Quiz.findById(req.params.id);
+        if (!quiz) {
+            return res.status(404).json({ error: 'Quiz not found' });
+        }
+        else{
+            console.log('quiz', quiz);
+            res.render('quiz', {quiz: quiz.toJSON()});
+            // res.status(200).json(quiz);
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));   
