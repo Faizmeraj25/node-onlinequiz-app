7c70619971ae686a8fbb3eafe2ce46e22b2dedaf
Sheikh Faiz Meraj <faizmeraj25@gmail.com>
2024-07-08 07:22:03 +0530

    added flash messages

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose')
 const Quiz = require('./models/quiz')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser'); 
+const session = require('express-session'); 
+const flash = require('connect-flash'); 
 const app = express(); 
 
 const dB = require('./config/keys').mongoURI;
@@ -15,6 +17,16 @@ app.use(express.static('public'));
 // Express body parser
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+//To display messages. 
+app.use(flash()); 
 
 
 //Connect to MongoDB
@@ -22,6 +34,17 @@ mongoose.connect(dB)
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('Error connecting to MongoDB:', error));
 
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+
+
+//Routers
 app.get('/', (req, res)=>{
     res.render('welcome'); 
 })
@@ -32,34 +55,50 @@ app.get('/create-quiz', (req, res)=>{
 app.post('/create-quiz',  async(req, res)=>{
     const { quizName, numQuestions, ...questionsData } = await req.body;
     console.log('req.body', quizName, numQuestions, questionsData);
-    const questions = [];
-
-    for (let i = 1; i <= numQuestions; i++) {
-        const questionText = questionsData[`question${i}`];
-        const options = [
-            { optionText: questionsData[`option${i}_1`] },
-            { optionText: questionsData[`option${i}_2`] },
-            { optionText: questionsData[`option${i}_3`] },
-            { optionText: questionsData[`option${i}_4`] }
-        ];
-        const correctOption = parseInt(questionsData[`correctOption${i}`], 10);
-
-        questions.push({ questionText, options, correctOption });
+    console.log('lenght', questionsData['question1']); 
+    let errors = []; 
+    if(!quizName)
+        errors.push({'msg' : 'Quiz Name is not entered'}); 
+    if(!numQuestions)
+        errors.push({'msg':'Number of Questions is not entered'}); 
+    if(questionsData['question1'] === undefined)
+        errors.push({'msg':'Click on Generate Question button'}); 
+    if(errors.length)
+    {
+        console.log('errors', errors);
+        res.render('create-quiz', {errors, quizName, numQuestions}); 
     }
+    else
+    {
+        const questions = [];
 
-    const newQuiz = new Quiz({
-        quizName,
-        numberOfQuestions: numQuestions,
-        questions
-    });
-    console.log(newQuiz); 
-    try {
-        const savedQuiz = await newQuiz.save();
-        console.log('Saved !!');
-        // res.status(201).json(savedQuiz);
-        res.redirect('/');
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+        for (let i = 1; i <= numQuestions; i++) {
+            const questionText = questionsData[`question${i}`];
+            const options = [
+                { optionText: questionsData[`option${i}_1`] },
+                { optionText: questionsData[`option${i}_2`] },
+                { optionText: questionsData[`option${i}_3`] },
+                { optionText: questionsData[`option${i}_4`] }
+            ];
+            const correctOption = parseInt(questionsData[`correctOption${i}`], 10);
+
+            questions.push({ questionText, options, correctOption });
+        }
+
+        const newQuiz = new Quiz({
+            quizName,
+            numberOfQuestions: numQuestions,
+            questions
+        });
+        console.log(newQuiz); 
+        try {
+            const savedQuiz = await newQuiz.save();
+            console.log('Saved !!');
+            req.flash('success_msg', `${quizName} quiz is created !!`); 
+            res.redirect('/quizzes');
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
 }); 
 app.get('/quizzes', async (req, res)=>{
